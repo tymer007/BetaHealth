82a3fab86ab210e53b952d29722c3ca9c1f39607
EnochScript <enochfolorunso2018@gmail.com>
2024-06-14 11:47:47 +0100

    Live Main

--- src/components/HomeHero.jsx
+++ src/components/HomeHero.jsx
@@ -8,7 +8,7 @@ const HomeHero = () => {
           <div className="hero-covering"></div>
           <div className="hero-writing">
           <h1>YOUR HEALTH, OUR PASSION:  TRUSTED GUIDIANCE FROM MEDICAL EXPERTS</h1>
-          <p>Welcome to Better Health  your trusted source for professional medical insights and personal wellness advice. Explore our extensive library of articles written by experienced medical journalists or book a consultation with certified doctors ready to assist you.</p>
+          <p>Welcome to Beta Health  your trusted source for professional medical insights and personal wellness advice. Explore our extensive library of articles written by experienced medical journalists or book a consultation with certified doctors ready to assist you.</p>
           <button className="hero-button">About Us</button>
           </div>
           <img src="./images/ACGhKNBLHzYusWMPFjNY2-transformed 1.png" alt="heroImage" />
